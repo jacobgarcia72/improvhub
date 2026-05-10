1599f876bc020913d694d1f0a9c2514f4d5ec700
Jacob Garcia <yockub@gmail.com>
2026-05-10 18:42:54 -0400

    WIP show dates

--- app/shows/new/date-inputs.tsx
+++ app/shows/new/date-inputs.tsx
@@ -1,6 +1,20 @@
 'use client'
+import Button from "@/components/form/button";
 import Input from "@/components/form/input";
 import { useState } from "react";
+
+function DateAndTime({ label = 'Day', index = 0 }) {
+    return (
+        <div className="flex flex-row flex-wrap gap-4">
+            <div>
+                <Input label={label} name={`date-${index}`} type="date" />
+            </div>
+            <div>
+                <Input label="Time" name={`time-${index}`} type="time" />
+            </div>
+        </div>
+    )
+}
 
 export default function DateInputs() {
     const [datesTBD, setDatesTBD] = useState<boolean>(false);
@@ -26,16 +40,20 @@ export default function DateInputs() {
                     min={1}
                     max={52}
                 />
-                {[...Array(Math.max(52, numberOfShowings))].map((x, index) => (
-                    <div key={index} className="flex flex-row flex-wrap gap-4">
-                        <div>
-                            <Input label="Day" name="date" type="date" disabled={datesTBD} />
-                        </div>
-                        <div>
-                            <Input label="Time" name="time" type="time" disabled={datesTBD} />
-                        </div>
+                <DateAndTime label={numberOfShowings > 1 ? 'Showing #1' : 'Date'} />
+                {numberOfShowings > 1 && <>
+                    <div className="flex flex-row">
+                        <p>Autofill:</p>
+                        <select>
+                            <option>Weekly</option>
+                            <option>Monthly</option>
+                        </select>
+                        <Button caption="Go" />
                     </div>
-                ))}
+                    {[...Array(Math.min(52, numberOfShowings - 1))].map((x, index) => (
+                        <DateAndTime key={index} label={`Showing #${index + 2}`} />
+                    ))}
+                </>}
             </>}
         </>
     )
