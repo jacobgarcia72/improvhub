b700b4fa627547488c66f1e09eb3e55ca3bb586b
Jacob Garcia <yockub@gmail.com>
2026-05-29 17:17:12 -0400

    styling

--- app/create/team/name-input.tsx
+++ app/create/team/name-input.tsx
@@ -9,7 +9,7 @@ export default function NameInput() {
     const [name, setName] = useState('');
     return (
         <div className="flex flex-row items-end">
-            <div className="w-6/7 pr-2">
+            <div className="w-[calc(100%-98px)] pr-2">
                 <Input required name="name" label="Team Name" onChange={setName} value={name} autocomplete={false} />
             </div>
             <div className="w-[80px]">
