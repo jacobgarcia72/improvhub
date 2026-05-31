531932810a1ac399b4ba59f9042a52953eb85002
Jacob Garcia <yockub@gmail.com>
2026-05-31 15:20:40 -0400

    added function to get pronoun form

--- lib/demographics.ts
+++ lib/demographics.ts
@@ -15,6 +15,26 @@ export const pronouns = [
     'any pronouns'
 ];
 
+export const getPronounForm = (pronouns: string, pronounFormIndex: number): string => {
+    const pronounForms: { [key: string]: string[] } = {
+        they: ['they', 'them', 'their', 'theirs'],
+        she: ['she', 'her', 'her', 'hers'],
+        he: ['he', 'him', 'his', 'his'],
+        ze: ['ze', 'zir', 'zir', 'zirs'],
+        xe: ['xe', 'xem', 'xyr', 'xyrs'],
+        ey: ['ey', 'em', 'eir', 'eirs'],
+        ve: ['ve', 'ver', 'vis', 'vers'],
+        ne: ['ne', 'nem', 'nir', 'nirs'],
+        fae: ['fae', 'faer', 'faer', 'faers']
+    }
+    let key = 'they';
+    const givenPronoun = pronouns.split(/\W+/)[0].toLowerCase();
+    if (Object.keys(pronounForms).includes(givenPronoun)) {
+        key = givenPronoun;
+    }
+    return pronounForms[key][pronounFormIndex]
+}
+
 export const genderIdentities = [
     'Male',
     'Female',
