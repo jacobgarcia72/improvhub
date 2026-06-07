85721b41a44667b30296b5c680c9cebdb6af052e
Jacob Garcia <yockub@gmail.com>
2026-06-07 13:50:55 -0400

    find teams and new team buttons

--- app/teams/page.tsx
+++ app/teams/page.tsx
@@ -1,9 +1,19 @@
+import Link from "next/link";
 import MyTeams from "./my-teams";
 import TeamInvitations from "./team-invitations";
+import Button from "@/components/form/button";
 
 export default function TeamsPage() {
     return <>
         <TeamInvitations />
         <MyTeams />
+        <section className="flex flex row gap-2">
+            <Link href="/create/team">
+                <Button caption="New Team" />
+            </Link>
+            <Link href="/search?for=teams">
+                <Button caption="Find Teams" />
+            </Link>
+        </section>
     </>
 }
