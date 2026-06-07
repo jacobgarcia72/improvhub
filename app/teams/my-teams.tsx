005f78f34bf1397ecee487353f6127c9029f93e4
Jacob Garcia <yockub@gmail.com>
2026-06-07 14:14:51 -0400

    display my teams on teams route

--- app/teams/my-teams.tsx
+++ app/teams/my-teams.tsx
@@ -1,7 +1,16 @@
-export default function MyTeams() {
+import MiniCard from "@/components/mini-card";
+import { getTeamsByUser } from "@/lib/teams";
+import { getCurrentUser } from "@/lib/users";
+
+export default async function MyTeams() {
+    const user = await getCurrentUser();
+    if (!user) return null;
+    const teams = await getTeamsByUser(user.id);
+    if (!teams?.length) return null;
     return (
         <section>
-            My Teams
+            <h2 className="px-3">My Teams</h2>
+            {teams.map((team, i) => <MiniCard key={i} item={team} type="team" />)}
         </section>
     )
 }
