ed77cdd62b2b351131dfad2a8af635838ee0536f
Jacob Garcia <yockub@gmail.com>
2026-06-21 18:37:05 -0400

    buy me a coffee

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/header";
 import { appName } from "@/lib/app-info";
+import BuyMeACoffee from "@/components/buy-me-a-coffee";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -34,6 +35,7 @@ export default function RootLayout({
         <main className="mt-12">
           {children}
         </main>
+        <BuyMeACoffee />
       </body>
     </html>
   );
---
+++ components/buy-me-a-coffee.tsx
@@ -0,0 +1,37 @@
+
+'use client'
+import { useEffect } from "react";
+
+export default function BuyMeACoffee() {
+    useEffect(() => {
+        const script = document.createElement("script");
+        const div = document.getElementById("buy-me-a-coffee");
+        script.setAttribute(
+            "src",
+            "https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js"
+        );
+        script.setAttribute("data-name", "BMC-Widget");
+        script.setAttribute("data-cfasync", "false");
+        script.setAttribute("data-id", "jacobgarcia25");
+        script.setAttribute("data-description", "Thank you for supporting!");
+        script.setAttribute(
+            "data-message",
+            "Buy me a coffee so I can continue developing this site!"
+        );
+        script.setAttribute("data-color", "#5F7FFF");
+        script.setAttribute("data-position", "Right");
+        script.setAttribute("data-x_margin", "18");
+        script.setAttribute("data-y_margin", "18");
+
+        script.onload = function () {
+            const event = new Event('DOMContentLoaded', { bubbles: true, cancelable: true });
+            window.dispatchEvent(event);
+        };
+        
+        div?.appendChild(script);
+
+    }, []);
+
+    return <div id="buy-me-a-coffee"></div>;
+}
+
