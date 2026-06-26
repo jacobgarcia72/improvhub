093a27019cf2a98cc8183167918e2fba9564c48b
Jacob Garcia <yockub@gmail.com>
2026-06-25 21:15:34 -0400

    discussions: show 'coming soon' for now

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -1,19 +1,24 @@
-import { getChatRooms } from "@/lib/chat"
-import { getCurrentUser } from "@/lib/users";
-import { redirect } from "next/navigation";
-import MessagesHeader from "./header";
-import MessagesBody from "./body";
-import { SearchParams } from "next/dist/server/request/search-params";
+// import { getChatRooms } from "@/lib/chat"
+// import { getCurrentUser } from "@/lib/users";
+// import { redirect } from "next/navigation";
+// import MessagesHeader from "./header";
+// import MessagesBody from "./body";
+// import { SearchParams } from "next/dist/server/request/search-params";
+import ComingSoon from "@/components/coming-soon";
 
-export default async function ChatPage({ searchParams }: { searchParams: Promise<SearchParams> }) {
-    const { room, topic } = await searchParams;
-    const user = await getCurrentUser();
-    if (!user) {
-        redirect(`/login?reroute=chat`);
-    }
-    const chatRooms = await getChatRooms(user.id);
-    return <>
-        <MessagesHeader chatRooms={chatRooms} />
-        <MessagesBody user={user} room={room as string || null} topic={topic as string || null} />
-    </>
+// export default async function ChatPage({ searchParams }: { searchParams: Promise<SearchParams> }) {
+//     const { room, topic } = await searchParams;
+//     const user = await getCurrentUser();
+//     if (!user) {
+//         redirect(`/login?reroute=chat`);
+//     }
+//     const chatRooms = await getChatRooms(user.id);
+//     return <>
+//         <MessagesHeader chatRooms={chatRooms} />
+//         <MessagesBody user={user} room={room as string || null} topic={topic as string || null} />
+//     </>
+// }
+
+export default async function ChatPage() {
+    return <ComingSoon />
 }
