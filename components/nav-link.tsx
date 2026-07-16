6140917dc5b7f00dfcb4614fdc480848de821d3b
Jacob Garcia <yockub@gmail.com>
2026-07-15 22:22:27 -0400

    fixed bug where feed navlink was highlighted when on feedback page

--- components/nav-link.tsx
+++ components/nav-link.tsx
@@ -10,7 +10,7 @@ export default function NavLink({ link, onClick }: {
     const path = usePathname();
     const href = `/${link.toLowerCase()}`;
     return (
-        <Link onClick={onClick || undefined} href={href} className={path.startsWith(href) ? 'active' : ''}>
+        <Link onClick={onClick || undefined} href={href} className={path === href || path.startsWith(`${href}/`) ? 'active' : ''}>
             <div className="flex flex-row min-w-14 px-2 justify-center text-[0.9rem]">
                 {{
                     'Feed': 'Live Feed',
