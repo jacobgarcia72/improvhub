1aed44ecf51a74cc2b39d1c95ce55c4504d18d0e
Jacob Garcia <yockub@gmail.com>
2026-07-20 19:47:13 -0400

    event tweaks and fixes

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -1,10 +1,9 @@
-// import { getChatRooms } from "@/lib/chat"
-// import { getCurrentUser } from "@/lib/users";
-// import { redirect } from "next/navigation";
-// import MessagesHeader from "./header";
-// import MessagesBody from "./body";
-// import { SearchParams } from "next/dist/server/request/search-params";
-import ComingSoon from "@/components/coming-soon";
+import { getChatRooms } from "@/lib/chat"
+import { getCurrentUser } from "@/lib/users";
+import { redirect } from "next/navigation";
+import MessagesHeader from "./header";
+import MessagesBody from "./body";
+import { SearchParams } from "next/dist/server/request/search-params";
 import { Metadata } from "next";
 import { appName } from "@/lib/app-info";
 
@@ -12,19 +11,15 @@ export const metadata: Metadata = {
     title: `Discussions | ${appName}`
 };
 
-// export default async function ChatPage({ searchParams }: { searchParams: Promise<SearchParams> }) {
-//     const { room, topic } = await searchParams;
-//     const user = await getCurrentUser();
-//     if (!user) {
-//         redirect(`/login?reroute=chat`);
-//     }
-//     const chatRooms = await getChatRooms(user.id);
-//     return <>
-//         <MessagesHeader chatRooms={chatRooms} />
-//         <MessagesBody user={user} room={room as string || null} topic={topic as string || null} />
-//     </>
-// }
-
-export default async function ChatPage() {
-    return <ComingSoon />
+export default async function ChatPage({ searchParams }: { searchParams: Promise<SearchParams> }) {
+    const { room, topic } = await searchParams;
+    const user = await getCurrentUser();
+    if (!user) {
+        redirect(`/login?reroute=chat`);
+    }
+    const chatRooms = await getChatRooms(user.id);
+    return <>
+        <MessagesHeader chatRooms={chatRooms} />
+        <MessagesBody user={user} room={room as string || null} topic={topic as string || null} />
+    </>
 }
