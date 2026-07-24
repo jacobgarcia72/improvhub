e62d1461d4f834a80791734d51b5bcf226d9c295
Jacob Garcia <yockub@gmail.com>
2026-07-23 23:07:28 -0400

    isDevOrStaging includes vercel preview env

--- lib/app-info.ts
+++ lib/app-info.ts
@@ -1,5 +1,5 @@
 export const appName = "ImprovLink";
 
 export const isDev = process.env.NODE_ENV === 'development';
-export const isDevOrStaging = process.env.NODE_ENV !== 'production';
-export const isProd = process.env.NODE_ENV === 'production';
+export const isDevOrStaging = isDev || process.env.NODE_ENV === 'test' || process.env.VERCEL_ENV === 'preview';
+export const isProd = process.env.NODE_ENV === 'production' && process.env.VERCEL_ENV !== 'preview';
