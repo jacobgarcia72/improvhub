c13d9db70725b631a13c5840685563cc5034c7e3
Jacob Garcia <yockub@gmail.com>
2026-07-23 22:47:30 -0400

    render discussions route in dev env only

--- app/discuss/layout.tsx
+++ app/discuss/layout.tsx
@@ -1,7 +1,8 @@
 import ComingSoon from "@/components/coming-soon"
+import { isDev } from "@/lib/app-info"
 
 export default function ChatLayout({ children }: { children: React.ReactNode }) {
-    if (process.env.NODE_ENV === 'production') {
+    if (!isDev) {
         return <ComingSoon />
     }
     return children
