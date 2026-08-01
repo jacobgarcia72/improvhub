a364f6079bc51773a3a5323e9bfa545cf51dd474
Jacob Garcia <yockub@gmail.com>
2026-07-31 23:21:17 -0400

    bugfix

--- app/discuss/new-comment-form.tsx
+++ app/discuss/new-comment-form.tsx
@@ -2,20 +2,25 @@
 import { postComment } from "@/actions/chat-actions";
 import Form from "@/components/form/form";
 import Text from "@/components/form/text";
-import { useState } from "react";
+import { useId, useState } from "react";
 import UserImage from "./user-image";
 import { User } from "@/types";
-import { getRandomNumberString } from "@/lib/helper-functions";
 
 export default function NewCommentForm({ user, room, topic, postId }: { user: User, room: string, topic: string, postId: string }) {
     const [active, setActive] = useState(false);
-    const [id] = useState(`comment-${getRandomNumberString(10)}`);
+    const id = useId();
 
     const handleSubmit = async (prevState: void | { message?: string }, formData: FormData) => {
         await postComment(user.id, room, topic, postId, prevState, formData);
         setActive(false);
     }
 
+    const focusCommentInput = () => {
+        setActive(true);
+        setTimeout(() => {
+            document.getElementById(id)?.querySelector<HTMLTextAreaElement>("#comment")?.focus();
+        }, 0);
+    }
 
     return (
         <div id={id} className="pl-2 pr-6 flex flex-row items-start justify-stretch w-full gap-1.5">
@@ -28,12 +33,7 @@ export default function NewCommentForm({ user, room, topic, postId }: { user: Us
                 </Form>
             </> : (
                 <div
-                    onClick={() => {
-                        setActive(true);
-                        setTimeout(() => {
-                            document.querySelector<HTMLTextAreaElement>(`#${id} #comment`)?.focus();
-                        }, 0);
-                    }}
+                    onClick={focusCommentInput}
                     className="w-full border dark:bg-black dark:text-mist-300 border-gray-300 rounded px-3 py-2 mb-3 text-mist-600 cursor-pointer"
                 >
                     <p>Comment...</p>
